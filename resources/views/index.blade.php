9ddc50e2ad010ce1d05d4d03ececabe54ee7b296
unknown <s0127wx@gmail.com>
2023-06-06 00:54:59 +0900

    test

---
+++ resources/views/index.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>青梅シニアイーグル</title>
+    <meta name="description" content="Title">
+    <link rel="icon" href="images/favicon.png" type="image/png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    @vite(['resources/sass/style.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <!-- gl-header -->
+    <header class ="gl-header">
+        <div class="container">
+            <nav class="gl-nav">
+                <ul>
+                    <li><a href="">お知らせ</a></li>
+                    <li><a href="">チーム紹介</a></li>
+                    <li><a href="">スケジュール</a></li>
+                    <li><a href="">試合結果</a></li>
+                    <li><a href="">新規会員募集</a></li>
+                    <li><a href="">問い合わせ</a></li>
+                </ul>
+                </nav>
+            <h1 class="top-main-header"><img src="/images/top.png" alt="青梅シニアイーグル"></h1>
+        </div>
+    </header>
+    <!-- gl-header -->
+
+    <!-- section-hero -->
+    <!-- <section class="section-hero">
+        <span class="pc"></span><img src="" alt="">
+        <span class="sp"></span><img src="" alt="">
+    </section> -->
+     <!-- section-hero -->
+
+    <!-- top-main -->
+     <main class="top-main">
+        <div class="container">
+            <p>お知らせ</p>
+    <!-- top-main -->
+
+    <!-- top-topics -->
+    <section class="top-topics">
+        <ul>
+
+            <li><a href="">リンク</a></li>
+        </ul>
+    </section>
+
+        </div>
+     </main>
+</body>
+</html>
